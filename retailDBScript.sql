57ab3eb2148d4c9791692798050eb64c32d08148
sandeep97trimbake <55341430+sandeep97trimbake@users.noreply.github.com>
2022-04-14 08:38:36 +0530

    Data Base Script
    import this script to your data  base IDE

---
+++ retailDBScript.sql
@@ -0,0 +1,78 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.12 
+MySQL - 5.5.19 : Database - retail
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`retail` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `retail`;
+
+/*Table structure for table `hibernate_sequence` */
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `hibernate_sequence` */
+
+insert  into `hibernate_sequence`(`next_val`) values (1);
+
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`userId`,`userName`) values
+ (1,'sandeep'),
+ (2,'rahul'),
+ (3,'rushi'),
+ (4,'suvarna');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `Pid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `price` bigint(25) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`Pid`),
+  KEY `FK_product` (`userId`),
+  CONSTRAINT `FK_product` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `product` */
+
+insert  into `product`(`Pid`,`name`,`type`,`category`,`price`,`userId`) values 
+(2,'DellEpson','laptop','electronic',35000,2),
+(3,'HP Pevilon','laptop','electronic',69000,1),
+(4,'H&M T-shirt','t-shirt','clothing',4000,4),
+(5,'addidas T-shirt','t-shirt','clothing',5000,4),
+(6,'sofa','chair','furniture ',5000,3),
+(7,'King Bed','Bed ','furniture ',16000,3);
+
+
